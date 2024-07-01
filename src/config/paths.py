f531b933194048d8a20d1a752c44b65663847b61
GSimonX37 <GSimonX37@gmail.com>
2024-07-01 21:42:53 +0700

    Fix: change the names of the path variables

--- src/config/paths.py
+++ src/config/paths.py
@@ -1,13 +1,12 @@
 PROJECT_PATH = r'C:\Projects\Letterboxd'
 
 DATA_PATH = PROJECT_PATH + r'\data'
-FILE_RAW_PATH = DATA_PATH + r'\raw'
-CHECKPOINT_PATH = DATA_PATH + r'\raw\checkpoints'
-FILE_PREPROCESSED_PATH = DATA_PATH + r'\processed'
+PATH_RAW_DATA = DATA_PATH + r'\raw'
+PATH_CHECKPOINT = DATA_PATH + r'\raw\checkpoints'
+PATH_PREPROCESSED_DATA = DATA_PATH + r'\processed'
 
-REPORTS_PATH = PROJECT_PATH + r'\reports'
-TRAIN_MODELS_REPORT_PATH = REPORTS_PATH + r'\training'
+PATH_REPORTS = PROJECT_PATH + r'\reports'
+PATH_TRAIN_REPORT = PATH_REPORTS + r'\training'
 
-TRAINED_MODELS_PATH = PROJECT_PATH + r'\models'
-
-MODELS_PATH = PROJECT_PATH + r'\src\ml\models'
+PATH_MODELS = PROJECT_PATH + r'\src\ml\models'
+PATH_TRAINED_MODELS = PROJECT_PATH + r'\models'
